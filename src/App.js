5dec32c8c60230f67924424a10fcfb4ec862b051
MinhChuoi <ngominh99it1@gmail.com>
2021-02-08 16:19:21 +0700

    commit4

--- src/App.js
+++ src/App.js
@@ -1,14 +1,43 @@
 import './App.css';
 import Top_products from './component/Top-products.js'
 import Item from './component/Item.js'
-import AddProduct from "./component/Add-product"
+import React, { useState } from 'react';
 
 function App() {
+  let [listItem, setListItem] = useState([
+    { productName: 'Inovex Television', productCode: 'PRD-01', brand: 'Innovex', supplier: 'Innovex Pvt' },
+    { productName: 'Plastic Chair 002', productCode: 'PRD-02', brand: 'Nika', supplier: 'Nikal' },
+    { productName: 'Washing Machiness', productCode: 'PRD-03', brand: 'Piuv', supplier: 'Piuv' },
+    { productName: 'Wasshing Fully Ab', productCode: 'PRD-04', brand: 'Reck', supplier: 'Reck Pbs' },
+    { productName: 'Giner Medium prov', productCode: 'PRD-05', brand: 'Yons', supplier: 'Yonsop' }
+  ]);
+
+  // listItem.map((item, index));
+
   return (
+    
+
     <div className="App container">
       <Top_products></Top_products>
 
-      <Item></Item>
+      <table id="list-item">
+        <tr>
+          <th>Product Name</th>
+          <th>Product Code</th>
+          <th>Brand</th>
+          <th>Supplier</th>
+          <th className="action">Actions</th>
+        </tr>
+        {
+          listItem.length>0 && listItem.map((item, index) =>
+          <Item 
+          key={item.productCode}
+          productName={item.productName}
+          productCode={item.productCode}
+          brand={item.brand}
+          supplier={item.supplier}
+        ></Item>)}
+      </table>
 
 
     </div>
--- src/component/Item.js
+++ src/component/Item.js
@@ -1,29 +1,18 @@
 import '../App.css';
 
-function Item() {
+function Item(props) {
+  // const {item} = this.props;
   return (
-    <div>
-      <table id="list-item">
-        <tr>
-          <th>Product Name</th>
-          <th>Product Code</th>
-          <th>Brand</th>
-          <th>Supplier</th>
-          <th className="action">Actions</th>
-        </tr>
-        <tr>
-          <td>LG 245s</td>
-          <td>PRD-01</td>
-          <td>Inovex</td>
-          <td>Inovex  </td>
-          <td className="action">
-            <a><img src="https://www.flaticon.com/svg/vstatic/svg/189/189664.svg?token=exp=1612716225~hmac=05eddf810dffb3a76328dfa7d0673135" height="22px"></img></a>
-            <a><img src="https://www.flaticon.com/svg/vstatic/svg/3616/3616796.svg?token=exp=1612716800~hmac=6c98f89515f4b97fa083d16b0c1282cb" height="22px"></img></a>
-            <a><img src="https://www.flaticon.com/svg/vstatic/svg/1214/1214926.svg?token=exp=1612716897~hmac=31fcb3e0887b27fe6dc54c875c6ca6e7" height="22px"></img></a>
-          </td>
-        </tr>
-      </table>
-    </div>
+    <tr>
+      <td>{props.productName}</td>
+      <td>{props.productCode}</td>
+      <td>{props.brand}</td>
+      <td>{props.supplier}</td>
+      <td className="action">
+        <button><img src="https://www.flaticon.com/svg/vstatic/svg/3616/3616796.svg?token=exp=1612716800~hmac=6c98f89515f4b97fa083d16b0c1282cb" height="22px"></img></button>
+        <button><img src="https://www.flaticon.com/svg/vstatic/svg/1214/1214926.svg?token=exp=1612716897~hmac=31fcb3e0887b27fe6dc54c875c6ca6e7" height="22px"></img></button>
+      </td>
+    </tr>
   );
 }
 
